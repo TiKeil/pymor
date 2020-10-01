24613706e1a2af9ddee43d934a683dec04b62694
Tim Keil <tim.keil@wwu.de>
2020-10-01 15:37:07 +0200

    add a simple demo script for linear optimization.

--- src/pymortests/demos.py
+++ src/pymortests/demos.py
@@ -40,6 +40,7 @@ DISCRETIZATION_ARGS = (
     ('burgers', ['--num-flux=simplified_engquist_osher', '0.1']),
     ('neural_networks', [50, 100, 20]),
     ('neural_networks_fenics', [30, 5]),
+    ('linear_optimization', [80, 25]),
     ('parabolic', ['heat', 1]),
     ('parabolic', ['heat', '--rect', 1]),
     ('parabolic', ['heat', '--fv', 1]),
