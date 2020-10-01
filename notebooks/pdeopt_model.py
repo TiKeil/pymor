c75ece37ca40276a043e0dbd3ba30c873cd5fb65
Tim Keil <tim.keil@wwu.de>
2020-10-01 13:39:13 +0200

    remove white spaces

--- notebooks/pdeopt_model.py
+++ notebooks/pdeopt_model.py
@@ -4,8 +4,8 @@ from pymor.models.basic import StationaryModel
 from pymor.operators.constructions import VectorOperator
 
 class LinearPdeoptStationaryModel(StationaryModel):
-    
-    def __init__(self, operator, rhs, output_functional=None, products=None, 
+
+    def __init__(self, operator, rhs, output_functional=None, products=None,
                  error_estimator=None, visualizer=None, name='LinearPdeoptModel'):
         super().__init__(operator, rhs, output_functional, products, error_estimator, visualizer, name)
         self.__auto_init(locals())
@@ -15,11 +15,11 @@ class LinearPdeoptStationaryModel(StationaryModel):
         if not hasattr(self, '_dual_model'):
             assert self.output_functional is not None
             assert self.output_functional.linear
-            assert 1 # TODO: how to assert that the operator is symmetric
+            assert 1 # TODO: assert that the operator is symmetric
             self._dual_model = self.with_(rhs=self.output_functional.H)
         return self._dual_model
-    
-    def solution_sensitivity(self, parameter, index, mu, U=None): 
+
+    def solution_sensitivity(self, parameter, index, mu, U=None):
         if U is None:
             U = self.solve(mu)
         residual_dmu_lhs = VectorOperator(self.operator.d_mu(parameter, index).apply(U, mu=mu))
@@ -37,11 +37,11 @@ class LinearPdeoptStationaryModel(StationaryModel):
         if adjoint_approach:
             if P is None:
                 P = self.solve_dual(mu)
-        for (parameter, size) in self.parameters.items(): 
+        for (parameter, size) in self.parameters.items():
             for index in range(size):
                 output_partial_dmu = self.output_functional.d_mu(parameter, index).apply(U, mu=mu).to_numpy()[0,0]
                 if adjoint_approach:
-                    residual_dmu_lhs = self.operator.d_mu(parameter, index).apply2(U, P, mu=mu)             
+                    residual_dmu_lhs = self.operator.d_mu(parameter, index).apply2(U, P, mu=mu)
                     residual_dmu_rhs = self.rhs.d_mu(parameter, index).apply_adjoint(P, mu=mu).to_numpy()[0,0]
                     gradient.append((output_partial_dmu + residual_dmu_rhs - residual_dmu_lhs)[0,0])
                 else:
